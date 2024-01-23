983e3fc28f6f8025429cc00885fe59057ff7552d
ptak18 <ptakyie@gmail.com>
2024-01-23 22:02:25 +0000

    added values and variables to script file

--- script.js
+++ script.js
@@ -5,12 +5,14 @@ const lon= 50
 const URL= `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${APIkey}`
 console.log(URL)
 
-fetch(URL, {
-    method: 'GET',
-    headers: { 'Content-Type': 'application/json' }
-})
+fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=metric&appid=${APIkey}`)
 .then(response => response.json())
-.then(data => console.log(data))
+.then(data => {
+    const windspeed = data.list[0].wind.speed
+    const humidity = data.list[0].main.humidity
+    const temp = data.list[0].main.temp
+    console.log(data.list[0])
+})
 .catch(error => console.error(error));
    
 // When a user views the current weather conditions for that city they are presented with:
